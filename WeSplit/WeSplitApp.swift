ca2c5f66ddf4fbc7a9859250c3a36028de76ccbd
Cathleen Vu <cathleendvu@gmail.com>
2024-07-26 21:51:14 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Cathleen Vu on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
