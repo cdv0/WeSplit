79bb959877d628d6c622cb3263b2b4d5c56b5fac
Cathleen Vu <cathleendvu@gmail.com>
2024-07-28 04:12:23 -0700

    Add comments

--- WeSplit/WeSplitApp.swift
+++ WeSplit/WeSplitApp.swift
@@ -2,6 +2,9 @@
 //  WeSplitApp.swift
 //  WeSplit
 //
+//  Input the check amount and select the tip percentage as well as the number of people you want to split the check between.
+//  You will be given the total that each person should contribute.
+//
 //  Created by Cathleen Vu on 7/26/24.
 //
 
